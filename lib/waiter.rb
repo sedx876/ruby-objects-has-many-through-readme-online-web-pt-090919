0696bfccde8fff7213d3e5de1b601cbb13c31e0e
Sharon Watkins <swatkins876@gmail.com>
2019-10-09 19:21:00 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
+  
+  @@all = []
 
+  attr_accessor :name, :yrs_experience 
+  
+  def initialize(name, yrs_experience)
+    @name = name 
+    @yrs_experience = yrs_experience 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
 end
